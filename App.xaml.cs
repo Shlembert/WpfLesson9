3a1dd29d4cd4214849b835e9ef2f6e466f5ffda9
Martin <martin@bimiboo.com>
2023-10-17 18:45:08 +0300

    prime

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace WpfLesson9
+{
+    public partial class App : Application
+    {
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace WpfLesson9
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplaySplitList(object sender, RoutedEventArgs e)
+        {
+            myListBox.ItemsSource = SplitText(textSplit.Text).ToArray();
+        }
+
+        private void DisplayReverse(object sender, RoutedEventArgs e)
+        {
+            myLabel.Content = ReverseWords(textReverse.Text);
+        }
+
+        private string[] SplitText(string text)
+        {
+            return text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private string ReverseWords(string inputPhrase)
+        {
+            string[] words = SplitText(inputPhrase);
+            Array.Reverse(words);
+
+            return string.Join(" ", words);
+        }
+
+    }
+}
